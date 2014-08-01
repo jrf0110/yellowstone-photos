42fffa84d4a450f498fb169e1216606e1461a629
John Fawcett <jrf0110@gmail.com>
2014-08-01 15:19:24 -0500

    redefine virtuals

--- config.js
+++ config.js
@@ -1,25 +1,14 @@
 var utils = require('./lib/utils');
 var config = {};
- 
-/**
- * Change configuration environment
- * @param  {String} env The environment to change to
- */
-var changeEnvironment = function(env){
-  if (env == null || !config.hasOwnProperty(env)) env = 'dev';
- 
-  for (var key in module.exports) delete module.exports[key];
- 
-  var _config = {};
- 
-  _config = utils.merge( utils.clone(config.default), config[env] );
- 
-  for (var key in _config) module.exports[key] = _config[key];
- 
-  module.exports.env = env;
-  
-  // Re-export this function since it was overwritten in the environment switch
-  module.exports.changeEnvironment = changeEnvironment;
+
+var virtuals = {
+  baseUrl: function(){
+    return [
+      this.isHttps ? 'https://' : 'http://'
+    , this.httpHost
+    , this.httpPort !== 80 ? (':' + this.httpPort) : ''
+    ].join('')
+  }
 };
  
 /**
@@ -30,13 +19,6 @@ config.default = {
   httpPort: process.env.YELLOWSTONE_APP_PORT || 3000
 , httpHost: 'localhost'
 , isHttps: false
-, get baseUrl() {
-    return [
-      this.isHttps ? 'https://' : 'http://'
-    , this.httpHost
-    , this.httpPort !== 80 ? (':' + this.httpPort) : ''
-    ].join('')
-  }
 , mediaDir: __dirname + '/public/media'
 , mediaPath: 'media'
 , mediaExtensions: [
@@ -67,6 +49,33 @@ config.production = {
 };
  
 module.exports = {};
+
+/**
+ * Change configuration environment
+ * @param  {String} env The environment to change to
+ */
+var changeEnvironment = function(env){
+  if (env == null || !config.hasOwnProperty(env)) env = 'dev';
+ 
+  for ( var key in module.exports ) delete module.exports[key];
+ 
+  var _config = {};
+ 
+  _config = utils.merge( utils.clone(config.default), config[env] );
+ 
+  for ( var key in _config ) module.exports[key] = _config[key];
+
+  for ( var key in virtuals ){
+    Object.defineProperty( module.exports, key, {
+      get: virtuals[ key ]
+    });
+  }
+ 
+  module.exports.env = env;
+  
+  // Re-export this function since it was overwritten in the environment switch
+  module.exports.changeEnvironment = changeEnvironment;
+};
  
 // Set the initial environment
 changeEnvironment( process.env.NODE_ENV || 'dev' );
